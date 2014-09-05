a700b83ea54115ea4ca3130817c02f0e3c9fc2a6
David Delmar <ddelmar@where.com>
2014-09-05 17:11:54 -0400

    adding volunteer pg

---
+++ volunteer/index.php
@@ -0,0 +1,47 @@
+<?php include_once("../includes/head.php") ?>
+<?php include_once("../includes/logo_nav.php") ?>
+
+
+<div class="hero"></div>
+<div id="container">
+
+
+
+<div id="tier2">
+	<section>
+		
+		<!-->
+		Things to bring up
+			Work as TA, not instructor
+			Not alone
+			Commitment to the youth
+		-->
+		<h2>Opportunities to mentor</h2>
+		<div class="product">
+			<h3>The Cohorts</h3>
+			<ol>
+<li><b>The Popup.</b> This is Resilient Coders Lite, so to speak. It's eight hours long, spread out over four weeks. They take place at a few schools or community centers around town, after school. This works as a feeder program for our summer program. The students spend their eight hours teaching themselves HTML and CSS as they build out their own websites.
+</li>
+<li><b>The School Year.</b> This cohort lasts a full school year, at a school. We're running our first one this year, starting in November. It's also one to two hours a week, around 3pm.
+</li>
+<li><b>Summer tech bootcamp.</b> This is 4 weeks over the summer, and "full-time," at nine to three. It's a little bit more freeform in that we can find a schedule that works best for you to come in and meet with your students while they work. 
+</li>
+</ol>
+
+<p>
+	However it is you choose to help, email me at <a href="mailto:david@resilientcoders.org">david@resilientcoders.org</a> and I will dispense one hand-stinging high five, like Erik's. The kind that makes people look to make sure everyone's okay.
+</p>
+
+</div><!-- /tier2 -->
+
+
+<?php include_once("../includes/footer.php") ?>
+</div><!-- container -->
+
+
+<script type="text/javascript" src="/j/nbw-parallax.js"></script>
+<script type="text/javascript" src="/j/jquery.localscroll-1.2.7-min.js"></script>
+<script type="text/javascript" src="/j/jquery.scrollTo-1.4.2-min.js"></script>
+<script type="text/javascript" src="/j/jquery.inview.js"></script>
+</body>
+</html>
