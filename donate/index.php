b9f6f49de3336acd6d3e54700eb65d6bb8aba01b
Tyler Mitchell <gravdanger510@gmail.com>
2014-11-06 00:01:18 -0500

    Began shop page

---
+++ donate/index.php
@@ -0,0 +1,25 @@
+<?php include_once("../includes/head.php") ?>
+<?php include_once("../includes/logo_nav.php") ?>
+
+<!--<div class="hero"></div>-->
+<div id="container">
+	<div id="tier2">
+	
+			<h2>Donate</h2>
+			<p>We work with Third Sector New England for our fiscal sponsorship, and as such are able to accept donations. Your donations will help us get in front of more kids and create more student mentors.</p>
+			<p>By using the donate buttons below, You'll receive a Resilient Coders t-shirt or hoodie as a thank you for your gift!</p>
+			<section id="shirts-container">
+				<div id="shirts">
+					<div class="merch" id="shirt-w"></div>
+					<div class="merch" id="shirt-r"></div>
+				</div>
+				<a class="donate-button" href="http://#">DONATE $20</a>
+			</section>
+	
+	</div>
+</div><!-- /container-->
+	
+	<?php include_once("../includes/footer.php") ?>
+		
+</body>
+</html>
