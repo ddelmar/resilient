b4afdc56d05fc5cf3819097532c6d9cb3f12f10c
David Delmar <ddelmar@where.com>
2014-12-01 10:10:20 -0500

    analytics

--- includes/footer.php
+++ includes/footer.php
@@ -7,3 +7,13 @@
       document.write(theYear)
 </script> Resilient Coders. 50 Milk Street, 17th Floor. Boston, MA 02109. <a href="mailto:david@resilientcoders.org">david@resilientcoders.org</a><span class="photocredit krysta">Photo by <a href="http://www.midnightdrifter.com" target="_blank" title="Krysta Voskowsky">Krysta Voskowsky</a></span>
 </div>
+<script>
+  (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
+  (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
+  m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
+  })(window,document,'script','//www.google-analytics.com/analytics.js','ga');
+
+  ga('create', 'UA-18042184-2', 'auto');
+  ga('send', 'pageview');
+
+</script>
