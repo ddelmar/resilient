c4b0662c7069ce6e9ecb5ef7cecd51e351ddfd8d
David Delmar <ddelmar@where.com>
2015-10-05 14:36:52 -0400

    edited, added hubweek

---
+++ hubweek/README.md
@@ -0,0 +1,1 @@
+# Hubweek
---
+++ map/index.php
@@ -0,0 +1,312 @@
+<?php include_once("../includes/head.php") ?>
+<?php include_once("../includes/logo_nav.php") ?>
+
+<?php /*
+
+::::::::::::::::::::::
+::: BEGIN CONTAINER ::
+::::::::::::::::::::::
+
+*/?>
+
+<div id="donate-modal" class="modal">
+	<div class="overlay"> </div>
+	<div class="detail zone1">
+		<h2>Del leaves PayPal</h2>
+		<p>
+			After having taken a number of vacation days to teach basic coding skills to incarcerated youth, Del leaves PayPal to launch Resilient Coders.
+		</p>
+	</div>
+	<div class="detail zone2">
+		<h2>The Police Step In</h2>
+		<p>
+		Resilient Coders is awarded grants through the Boston Police Department, for our work with high-risk youth.
+		Our city is lucky enough to have a police force that believes in finding alternate ways to reduce violence in our communities.
+		They believe as we do: Careers over cuffs.
+	</p>
+	</div>
+	<div class="detail zone3">
+		<h2>TUGG</h2>
+		<img src="i/map4_tugg.JPG" />
+		<p>
+			Resilient Coders is invited to join the <a href="http://tugg.org" target="_blank">TUGG</a> portfolio. As a member
+			company, we're invited to compete at events at which attendees are asked to vote for a grantee from among
+				competing companies. Resilient Coders wins Tech Gives Back in September 2014. We would later go on to win
+				again in April, and again the following September.
+		</p>
+		<p>Shout outs to the TUGG team, and to its (then) Executive Director David Brown, who continues to be an immensely
+			valuable ally and advisor. Special thanks to our teams of hustlers who donned the skull t-shirt and helped us
+			win those events.
+		</p>
+	</div>
+	<div class="detail zone4">
+		<h2>We become legit</h2>
+		<p>
+			The day after winning TUGG Tech Gives Back, Resilient Coders becomes incorporated, thanks to the efforts of our
+			allies Goulston Storrs. Our friends at Goulston would go on to help us achieve our 501(c)(3) status, and set up
+			our legal operations. Matthew Woodbury gets a special shout out for turning an "advice" phone call into a real, tangible,
+			and invaluable partnership.
+		</p>
+		<p>
+			Eight months later, the IRS would join the throngs of people misspelling our name.
+		</p>
+
+	</div>
+	<div class="detail zone5">
+		<h2>The Board of Superhumans</h2>
+		<p>
+			Del finds three people with that rare blend of experience, grit, passion, and <i>com</i>passion.
+			As Resilient Coders itself bridges the worlds of social justice and technology, so too does its board.
+			Joshua Summers, the board's chair, is a successful serial entrepreneur, investor, and active presence in Boston's tech community.
+			Susanne Beck offers her vast experience consulting with non-profits in periods of transition and growth.
+			Pamela Aldsworth, manager of client relations at Silicon Valley Bank and avid supporter of TUGG, has experience
+			in both tech and meaningful philanthropy.
+		</p>
+		<p>Around this time, three Resilient Coders attended the MassTLC UnConference, thanks to their sponsorship.
+			Our Youth Dev Lead, Micah, pitched a session on code literacy in the inner city. It was at this session that our hackers
+			met Chris Swenor, CEO of <a href="http://eastcoastproduct.com" target="_blank">East Coast Product,</a> who would ultimately become our Technical Advisor.
+		</p>
+	</div>
+
+	<div class="detail zone6">
+		<h2>Resilient Coders blows up</h2>
+		<img src="i/map7_grovehall.jpg" />
+		<p>
+			Over the course of our first year in operation, with help from our allies, Resilient Coders reaches roughly 80 young people. We run
+			small cohorts in four public schools, two community centers, two correctional institutions, and two coding camps.
+		</p>
+		<p>
+			In every cohort, we ran deliberate and measurable tests, in order to develop a real pedagogy "on the ground."
+		</p>
+	</div>
+
+	<div class="detail zone7">
+		<h2>We Launch "Coworking"</h2>
+		<img src="i/map8_cic.jpg" />
+		<p>
+			Thanks to our partnership with <a href="http://cic.us" target="_blank">the Cambridge Innovation Center (CIC),</a>
+			we launch our ongoing "Coworking" Cohort, right downtown. In this space, our hackers work elbow to elbow
+			with tech innovators, and develop real relationships with them. Coworking allows us to provide our youth
+			with an immersive learning environment.
+		</p>
+		<p>
+			We want to thank CIC for their support. And we want to mention that in February, we had young people whose
+			schools had closed due to snow, and <i>still</i> they would make the trek down to CIC, if we were open.
+		</p>
+	</div>
+
+	<div class="detail zone8">
+		<h2>We send two hackers to Startup Institute</h2>
+		<iframe src="https://www.youtube.com/embed/JU1UunsFgpU" frameborder="0" allowfullscreen></iframe>
+
+		<p>
+			<a href="http://startupinstitute.com" target="_blank">Startup Institute</a> is a competitive 8-week
+			bootcamp focused on providing its students with the skills they need to work for a startup.
+			Fredy and Micah were able to attend, thanks in no small part to scholarships from Startup Institute,
+			Yelp, and a successful crowdfunding campaign. Above is our crowdfunding video.
+		</p>
+		<p>
+			The last day of the crowdfunding campaign, someone who had already donated checked back in, to see how
+			we'd fared. We were short $200. He donated again, covering the balance.
+		</p>
+		<p>
+			He was a Startup Institute alum. He wanted Fredy and Kai to join his community.
+		</p>
+		<p>Shout out to Andy Laub of <a href="http://asithappens.tv" target="_blank">As It Happens Films</a>
+			 for the incredible video. (Seriously though.)
+		</p>
+
+	</div>
+
+
+	<div class="detail zone9">
+		<h2>Boston Coding Camp</h2>
+		<iframe src="https://www.youtube.com/embed/ASKzlgmopyo" frameborder="0" allowfullscreen style="width:100%; height:400px"></iframe>
+
+		<p>
+			Boston Coding Camp launches over spring break, in partnership with <a href="http://typp.org">The Young People's Project.</a>
+			25 young people built their first websites and presented them to an audience of technologists.
+		</p>
+		<p>
+			Thanks to our speakers, our mentors, and <a href="/camp">our sponsors</a> who made this all possible.
+		</p>
+		<p>Shout out to Andy Laub of <a href="http://asithappens.tv" target="_blank">As It Happens Films</a>
+			 for the incredible video.
+		</p>
+
+	</div>
+
+
+	<div class="detail zone10">
+		<h2>Micah lands a job at the Boston Globe</h2>
+		<img src="i/map10_kai.jpg" />
+		<p>
+			Our man Micah (Kai) lands a full-time software engineering job at the Boston Globe, at the age of 19.
+			He still joins us at Resilient, as a mentor.
+		</p>
+		<p>Shout out to Andy Laub of <a href="http://asithappens.tv" target="_blank">As It Happens Films</a>
+			 for the photo.
+		</p>
+
+	</div>
+
+
+	<div class="detail zone11">
+		<h2>Resilient Lab launches with Wanderu</h2>
+		<p>
+			Resilient Lab is the web design and development shop that employs the hackers who are ready for that challenge.
+			We launched earlier than expected, with an awesome pilot client, Wanderu. Fredy designed the user interface
+			for their iPhone app. You can read about this app in <a href="http://time.com/3714747/iphone-apps-snl/" target="_blank">TIME.</a>
+			It hit #1 for the travel category the week it launched. And if you're not using it to book your ground travel,
+			you're doing it wrong.
+		</p>
+		<p>
+			Thank you to Wanderu for taking a chance on us.
+		</p>
+
+	</div>
+
+
+
+	<div class="detail zone12">
+		<h2>We win the TUGG Wine and Tequila Party</h2>
+		<img src="i/map12_tugg.jpg" />
+		<p>
+			This time, we roll in with our young people, and let them hustle.
+			Resilient Coders wins the Wine and Tequila Party, and would later go on to win
+			again the following September.
+		</p>
+		<p>Shout outs to the TUGG team, and special thanks to our teams of hustlers who donned
+			the skull t-shirt and helped us win again.
+		</p>
+		<p>By the way... Fredy's map shows him lifting Del up over his shoulder. That happened.
+		</p>
+		<img src="i/map12_fredy.JPD" />
+
+	</div>
+
+
+
+	<div class="detail zone13">
+		<h2>Resilient Coders joins the MassChallenge cohort</h2>
+		<img src="i/map12_tugg.jpg" />
+		<p><a href="http://masschallenge.org">MassChallenge</a> is the world's largest tech accelerator program.
+			We're selected as one of the 128 MassChallenge companies in Boston, out of 2,300 applicants
+			from around the world.
+		</p>
+		<p>We're humbled by the honor, and grateful for the enormous benefits that have come from MassChallenge -- especially
+			the people we've met, who have volunteered their time and wisdom to help us improve.
+		</p>
+		<p>While at MassChallenge, two of our young people helped to build our curriculum platform, Scoreboard.
+			They would come into the MassChallenge space, learn React, and then build. Special thanks to the Boston
+			Police Department for funding this initiative, and to <a href="http://eastcoastproduct.com" target="_blank">East Coast Product</a>
+			for their patience, guidance, and generosity of time.
+		</p>
+	</div>
+
+
+	<div class="detail zone14">
+		<h2>Media Leadership Institute</h2>
+		<img src="i/map15_mli.jpg" />
+		<p>
+			Resilient Coders co-hosts the Media Leadership Institute over the summer, with our friends at
+			<a href="http://www.presspasstv.org/" target="_blank">Press Pass TV</a>, designing and building a functional
+			prototype	for the Mayor's Office. Our hackers craft business plans, assess the problems presented to them
+			(snow removal and youth unemployment), and ultimately respond with something akin to an "uber" of snow shoveling.
+		</p>
+		<p>
+			We have many people to thank for this one. Thanks to <a href="/camp">our sponsors</a> for the event,
+			including ActBlue and Sam Adams.
+			Thanks to OpenView partners, who listened to the pitches halfway through the program, offered up their
+			invaluable feedback, and even
+			<a href="http://blog.openviewpartners.com/resilient-coders/" target="_blank" title="Why Resilient Coders Has OpenView Excited About the Future of Boston Tech">
+wrote some very kind things about us.
+</a>
+			Thanks to all the mentors who volunteered their time, especially the <a href="http://testive.com" target="_blank">Testive</a>,
+			who showed up every week, ready to work. Thanks to Eastern Bank for the use of their space,
+			to Meghan Joyce of Uber for speaking, and of course to our fantastic partners, Press Pass TV.
+		</p>
+	</div>
+
+
+
+</div>
+
+
+
+<div class="spacer"  style="padding: 0;">
+	<div class="container">
+		<div id="tier2" style="padding-bottom:0;">
+
+		<h2>Year One</h2>
+
+		<p class="intro" style="border:0;">
+		It's been pretty wild. This map, drawn by our own Fredy Melo, calls out some of our pivotal moments. Click around
+		to learn more.
+
+
+		</p>
+	</div><!-- /tier2 -->
+	</div><!-- /container -->
+
+		<div id="map">
+			<div id="zone1"></div>
+			<div id="zone2"></div>
+			<div id="zone3"></div>
+			<div id="zone4"></div>
+			<div id="zone5"></div>
+			<div id="zone6"></div>
+			<div id="zone7"></div>
+			<div id="zone8"></div>
+			<div id="zone9"></div>
+			<div id="zone10"></div>
+			<div id="zone11"></div>
+			<div id="zone12"></div>
+			<div id="zone13"></div>
+			<div id="zone14"></div>
+
+		</div><!-- /map -->
+
+
+
+
+
+</div><!-- /spacer -->
+
+<? /*
+	The local reference to jQuery library is temporary
+*/ ?>
+<script type="text/javascript" src="jquery.js" ></script>
+
+<script>
+$(document).ready(function(){
+hotSpot();
+var docwidth = $(document).width();
+$("#map").height(docwidth);
+
+$('#map div').click(function () {
+	var thisID = $(this).attr("id");
+	$('#donate-modal').addClass('open');
+	$(".detail." + thisID).show();
+	$(".detail:not(." + thisID + ")").hide();
+		$('html, body').animate({
+			scrollTop: $('#donate-modal').offset().top
+		}, 500);
+});
+
+$('.overlay').click(function() {
+	$('.modal').removeClass('open');
+});
+});
+
+
+function hotSpot(){
+	var marginTop = $((document).width()*0.5);
+	// $("#map > div").css("margin-top", 600);
+}
+$window.resize(function(){
+	hotSpot();
+});
+</script>
+
+<?php include_once("../includes/footer.php") ?>
