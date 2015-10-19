157a8b7a9dc40346b723c0ae41f07987fd6865b1
David Delmar <ddelmar@where.com>
2015-10-19 16:15:19 -0400

    minor content edits

--- lab/index.php
+++ lab/index.php
@@ -10,9 +10,16 @@
 Our most experienced students operate a web design and development studio. Our young coders work with clients to build a microsite or web application. Resilient Lab represents an opportunity for both the student and the client. Students gain real, practical, hands-on experience. And clients are able to give back to the community in a way that benefits their business.
 </p>
 <p>
-Our youth are trained to build websites, and enjoy the guidance and support of professionals.
-We've designed the UI for a successful iPhone app, built websites, custom Wordpress themes, and built a curriculum platform in React. We can do it.
-Email David Delmar at <a href="david@resilientcoders.org">david@resilientcoders.org</a> to begin scheming.
+	<b>What sort of stuff can you do?</b> Our young people are most comfortable with relatively simple websites, and custom Wordpress theming. But our partnership with East Coast Product allows us to do pretty much anything. We've teamed up to build software using React, for example. Our friends at ECP delegated out to our students the work that was appropriate to their skills; a set that grew over the course of the project. Whether or not we're working with ECP on a particular project, every line of code that goes out is vetted by a professional. We hold our youth to professional standards.
+</p>
+<p>
+	<b>How does the money work?</b> Basically, half of the money that we make on a Resilient Lab project goes to pay for the wages of those who worked on it. The other half is invested in their education. The idea is that a young person will be hired to build something that lies just outside the realm of his expertise. He will be paired up with a more senior-level person who then delegates skill-appropriate work to the young person, and mentors him throughout the job. They may spend an hour learning, for every hour spent working. Our quote covers the instructor's time, and the youth coder's time, with the understanding that half of their time was spent learning.
+</p>
+<p>
+	<b>So, can I write it off my taxes as a donation?</b> Some of it, yes. Your invoice will itemize out the straight cost of building your project, versus the investment you're making in our coders' education. Within the hourly rate that you're quoted, usually half of that is a donation, and will be spent on training the coders to be ready to work on your project. The other half is your payment for a service. This will vary depending on the specific needs of a project. We'll be transparent about those proportions when we start.
+</p>
+<p>
+	<b>How do we get started?</b> Drop Tyler a note at <a href="mailto:tyler@resilientcoders.org">tyler@resilientcoders.org</a> to begin scheming.
 </p>
 
 
