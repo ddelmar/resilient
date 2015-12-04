637f9367e6b4b45de8c148455c00bb801ab6792d
David Delmar <ddelmar@where.com>
2015-12-04 13:03:24 -0500

    hour of code

---
+++ includes/hourofcode.php
@@ -0,0 +1,21 @@
+<div id="micahvideo">
+	<img src="https://resilientcoders.s3.amazonaws.com/i/x.gif" id="x">
+	<div id="video">
+		<h1>12.12.2015</h1>
+		<img src="https://resilientcoders.s3.amazonaws.com/i/hoc_tyril.jpg" />
+		<p>
+			We're thrilled to be participating in the Museum of Science's "Hour of Code" expo this Saturday, from 10 - 4. We've been working together to come up with a pretty cool activity: Our guys will help you express your idea for social change, as expressed in JavaScript.
+		</p>
+		<p>
+Swing by, keep an eye out for the blindingly good-looking gentlemen in skull t-shirts. Meet Tyril, whose statement is above, and the rest of the gang. Participate in all of the other awesome activities going on at the MoS, which, incidentally, gets exponentially cooler with every passing day. Thanks to our allies at Infosys Foundation USA, Microsoft, and of course the Museum of Science, for making this happen. Hour of Code is an initiative of code.org, and you should check them out and do every single activity on there. Right now.
+		</p>
+		<p>
+		Sadly, Tyril is not old enough for you to write him into your ballot in November.
+
+
+		</p>
+
+
+	</div><!-- /video -->
+	<div id="black"></div>
+</div><!-- /micahvideo -->
